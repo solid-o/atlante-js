afb2e5bdc7f8257bb08184fcecef31cb0e58782a
Alessandro Chitolina <alekitto@gmail.com>
2021-11-13 15:07:44 +0100

    fix definition references

--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
+/// <reference types="@jymfony/exceptions" />
+/// <reference types="@jymfony/contracts" />
+/// <reference types="@jymfony/util" />
+
 /// <reference path="lib/Exception/AbstractException.ts" />
 /// <reference path="lib/Exception/AccessDeniedException.ts" />
 /// <reference path="lib/Exception/BadRequestException.ts" />
