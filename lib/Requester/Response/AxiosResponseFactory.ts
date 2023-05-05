b90320f887d23b4e04aaa60af1dc8e659a259bb8
Alessandro Chitolina <alekitto@gmail.com>
2023-05-05 12:33:37 +0200

    fix: force override of axios headers type

--- lib/Requester/Response/AxiosResponseFactory.ts
+++ lib/Requester/Response/AxiosResponseFactory.ts
@@ -8,7 +8,7 @@ class AxiosResponseFactory extends AbstractResponseFactory {
     fromResponse(response: AxiosResponse): ResponseInterface {
         return this.makeResponse(
             response.data,
-            new Headers(response.headers),
+            new Headers(response.headers as unknown as Record<string, string | string[]>),
             response.status
         );
     }
